25d9d9114aa6bee23a7e2fe2e4d33e1ac10a500b
Tomasz Zeman <tomasz.zeman@gmail.com>
2021-08-11 21:03:35 +1000

    Update main.go
    Print more from runtime

--- main.go
+++ main.go
@@ -7,5 +7,10 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
-	fmt.Printf("Platform OS is: %s\n", runtime.GOOS)
+	fmt.Println("-----------------------------")
+	fmt.Printf("\nGOOS:%s", runtime.GOOS)
+	fmt.Printf("\nGOARCH:%s", runtime.GOARCH)
+	fmt.Printf("\nGOROOT:%s", runtime.GOROOT())
+	fmt.Printf("\nCompiler:%s", runtime.Compiler)
+	fmt.Printf("\nNo. of CPU:%d", runtime.NumCPU())	
 }
